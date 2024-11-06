5033f9d8e768741b194bfa8f89c448b10fc349bd
Kattushkaa <kattushkaa@gmail.com>
2024-11-06 16:51:36 +0900

    Second init

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import Header from './Header';
 import Nav from './Nav';
@@ -17,29 +16,3 @@ function App() {
 }
 
 export default App;
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App"> Homepage
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> 6c6e7bcc6341c8dc9cf87a0bf6ecbeab58b02484
