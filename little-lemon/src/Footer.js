2f38bc85b8d27810b9bbc2d8bfa868e36b571a0b
Kattushkaa <kattushkaa@gmail.com>
2024-11-06 16:40:59 +0900

    Initial commit

---
+++ little-lemon/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import footer from './footer.png';
+
+function Footer() {
+  return (
+    <footer>
+        <img src={footer} alt="Little Lemon Footer" />
+      <p>© 2024 Little Lemon. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
